4f2aa807c353f928a9260333812f7fc13e745aae
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-04-30 22:25:33 +0700

    feat(register): add recycle validator

--- src/main/java/com/quangta/exception/ErrorCode.java
+++ src/main/java/com/quangta/exception/ErrorCode.java
@@ -16,12 +16,12 @@ public enum ErrorCode {
     UNCATEGORIZED_EXCEPTION(9999, "Uncategorized exception", HttpStatus.INTERNAL_SERVER_ERROR),
     USER_EXISTED(1001, "User has already existed!", HttpStatus.BAD_REQUEST),
     INVALID_MESSAGE_KEY(1002, "Invalid message key", HttpStatus.BAD_REQUEST),
-    USERNAME_INVALID(1003, "Username must be at least 3 characters!", HttpStatus.BAD_REQUEST),
-    PASSWORD_INVALID(1004, "Password must be at least 8 characters!", HttpStatus.BAD_REQUEST),
+    USERNAME_INVALID(1003, "Username must be at least {min} characters!", HttpStatus.BAD_REQUEST),
+    PASSWORD_INVALID(1004, "Password must be at least {min} characters!", HttpStatus.BAD_REQUEST),
     USER_NOT_EXISTED(1005, "User not exist!", HttpStatus.NOT_FOUND),
     UNAUTHENTICATED(1006, "Unauthenticated!", HttpStatus.UNAUTHORIZED),
     UNAUTHORIZED(1007, "Unauthorized!", HttpStatus.FORBIDDEN),
-    INVALID_DOB(1008, "Invalid date of birth!", HttpStatus.BAD_REQUEST),
+    INVALID_DOB(1008, "Your age must be at least {min}", HttpStatus.BAD_REQUEST),
     ;
     int code;
     String message;
--- src/main/java/com/quangta/exception/GlobalExceptionHandler.java
+++ src/main/java/com/quangta/exception/GlobalExceptionHandler.java
@@ -1,14 +1,23 @@
 package com.quangta.exception;
 
 import com.quangta.dto.response.ApiResponse;
+import jakarta.validation.ConstraintViolation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.Map;
+import java.util.Objects;
+
+
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final static String  MIN_ATTRIBUTE = "min";
 
     @ExceptionHandler(value = Exception.class)
      ResponseEntity<ApiResponse> handleRuntimeException() {
@@ -54,16 +63,36 @@ public class GlobalExceptionHandler {
 
         ErrorCode errorCode = ErrorCode.INVALID_MESSAGE_KEY;
 
+        Map<String, Object> attributes = null;
+
         try {
             errorCode = ErrorCode.valueOf(enumKey);
+            
+            var constraintViolations = e.getBindingResult()
+                    .getAllErrors()
+                    .get(0)
+                    .unwrap(ConstraintViolation.class);
+
+            attributes = constraintViolations.getConstraintDescriptor().getAttributes();
+
+            log.info(attributes.toString());
+
         } catch (IllegalArgumentException exception){
 
         }
         ApiResponse apiResponse = new ApiResponse();
 
         apiResponse.setCode(errorCode.getCode());
-        apiResponse.setMessage(errorCode.getMessage());
+        apiResponse.setMessage(Objects.nonNull(attributes) ?
+                mapAttribute(errorCode.getMessage(), attributes)
+                : errorCode.getMessage());
 
         return ResponseEntity.badRequest().body(apiResponse);
+    }
+
+    private String mapAttribute(String message, Map<String, Object> attributes){
+        String minValue = String.valueOf(attributes.get(MIN_ATTRIBUTE));
+
+        return message.replace("{" + MIN_ATTRIBUTE + "}", minValue);
     }
 }
