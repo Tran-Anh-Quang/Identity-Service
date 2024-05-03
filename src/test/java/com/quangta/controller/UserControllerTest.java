3e476ad6fdfb5fb20b2281c6a2f33ab9917be773
Tran Anh Quang <dinosaurous9x@gmail.com>
2024-05-03 23:49:43 +0700

    test: test user controller

---
+++ src/test/java/com/quangta/controller/UserControllerTest.java
@@ -0,0 +1,15 @@
+package com.quangta.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Slf4j
+@SpringBootTest
+public class UserControllerTest {
+
+    @Test
+    void createUser() {
+        log.info("hello");
+    }
+}
